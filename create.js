5f61279b0db7455e5df3f2712ff23a1517f2832a
so1omon <solmin3665@gmail.com>
2022-04-07 14:59:55 +0900

    'awfeawf'

--- create.js
+++ create.js
@@ -258,7 +258,7 @@ const login = async()=>{
                 url:link
             },
             success:function(result){
-                window.location.replace(result.url);
+                window.location.renodeplace(result.url);
             },
             error:function(result){
                 Swal.fire({
