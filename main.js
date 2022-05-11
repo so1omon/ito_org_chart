b2d69d9c77e7b5f677c8061a6d11d16bed8f94a4
so1omon <solmin3665@gmail.com>
2022-05-11 09:03:26 +0900

    conn.end() 추가

--- main.js
+++ main.js
@@ -104,7 +104,7 @@ app.post('/login',(req,res)=>{
             res.json({error:"Password is not correct."});
         }
     });
-    
+    conn.end();
 });
 app.get('/logout', (req, res)=>{
     
@@ -129,6 +129,7 @@ app.get('/logout', (req, res)=>{
         console.log('로그인 상태 아님');
         res.redirect('/');
     }
+    conn.end();
 });
 app.get('/', (request, response)=>{ // http://[host]:[port]/로 접속 시 나올 페이지
     conn=db_config.init();//db connection handler 가져오기
@@ -423,8 +424,9 @@ app.post('/delete/:emp_id', function(req, res){ // 배치된 사용자의 seat_a
             }
         });
 
-        conn.end();
         res.json({result:'성공'});
+        
+        conn.end();
     }
 });
 
